5fc800650ac0345f9a104206b802f05f0a95032d
tifani <tifanisds@gmail.com>
2025-06-02 10:30:14 -0300

    Feat: Cria a página de resultado e seus componentes

---
+++ src/components/about-temperaments/colerico.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h2>Temperamento: Colérico</h2>
+        <div class="circle">
+            <img src="../../assets/img/colerico.svg" alt="">
+        </div>
+        <p class="about-temperament">
+            Pessoas coléricas são determinadas, líderes naturais e orientadas para resultados. Gostam de desafios, tomam decisões rápidas e possuem uma energia intensa para realizar o que querem.
+        </p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "/src/styles/variables.scss" as *;
+
+    .container {
+        width: 90%;
+        flex-direction: column;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        h2 {
+            font-size: 20px;
+            font-weight: 600;
+        }
+
+        .circle {
+            margin: 20px 0 20px 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 206px;
+            height: 206px;
+            border-radius: 100px;
+            background-color: $main-title-color;
+        }
+
+        p {
+            font-size: 14px;
+            text-align: center;
+            font-weight: 500;
+        }
+
+    }
+
+</style>
---
+++ src/components/about-temperaments/sanguineo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <h2>Temperamento: Sanguíneo</h2>
+        <div class="circle">
+            <img src="../../assets/img/sanguineo.svg" alt="">
+        </div>
+        <p class="about-temperament">
+            Extrovertido, carismático e otimista, o sanguíneo se destaca pela facilidade de se comunicar e se conectar com os outros. Ama viver experiências novas e é movido por emoções e entusiasmo.        </p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "/src/styles/variables.scss" as *;
+
+    .container {
+        width: 90%;
+        flex-direction: column;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        h2 {
+            font-size: 20px;
+            font-weight: 600;
+        }
+
+        .circle {
+            margin: 20px 0 20px 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 206px;
+            height: 206px;
+            border-radius: 100px;
+            background-color: $main-title-color;
+        }
+
+        p {
+            font-size: 14px;
+            text-align: center;
+            font-weight: 500;
+        }
+
+    }
+
+</style>
